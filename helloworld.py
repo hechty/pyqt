749ab425cbd218eac33da7a33a55a86abf6ef186
hechty <hchw1230@qq.com>
2017-05-06 22:15:09 +0800

    第一个pyqt4程序

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,16 @@
 #! /usr/bin/env python3
+import sys
+from PyQt4 import QtCore, QtGui
+
+class startQT4(QtGui.QMainWindow):
+    def __init__(self, parent = none):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_notepad()
+        self.ui.setUi(self)
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = StartQT4()
+    myapp.show()
+    sys.exit(app.exec_())
