00a1c5c0cddecf555da200f853f03da660295abd
hechty <hchw1230@qq.com>
2017-05-09 21:49:05 +0800

    Signals and slot

---
+++ 4-Signals3.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+
+import sys
+from PyQt4 import QtGui, QtCore
+
+
+class Example(QtGui.QMainWindow):
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+        btn1 = QtGui.QPushButton("Button1", self)
+        btn1.move(30, 50)
+        
+        btn2 = QtGui.QPushButton("Button2", self)
+        btn2.move(150, 50)
+
+        btn1.clicked.connect(self.buttonClicked)
+        btn2.clicked.connect(self.buttonClicked)
+
+        self.statusBar()
+
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle("tEST")
+        self.show()
+
+    def buttonClicked(self):
+        sender = self.sender()
+        self.statusBar().showMessage(sender.text() + ' was pressed')
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
+        
