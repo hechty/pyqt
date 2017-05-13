2e8bda06cd51d347d4239254a965d70889addaf5
hechty <hchw1230@qq.com>
2017-05-13 16:49:41 +0800

    colordialog

---
+++ 5-dialog.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+
+import sys
+from PyQt4 import QtGui
+
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.btn = QtGui.QPushButton('dialog', self)
+        self.btn.move(20, 20)
+        self.btn.clicked.connect(self.showDialog)
+
+        self.le = QtGui.QLineEdit(self)
+        self.le.move(130, 22)
+
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('INput Dialog')
+        self.show()
+
+    def showDialog(self):
+        text, ok = QtGui.QInputDialog.getText(self, 'Inputl Dialog', 'eNTER YOUR NAME')
+
+        if ok:
+            self.le.setText(str(text))
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
